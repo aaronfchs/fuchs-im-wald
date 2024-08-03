4832a8c7a04426958335176388afb1f0a728a282
aaronfchs <aaron.fuchs@posteo.de>
2024-08-03 17:45:23 +0200

    update statistic-page

--- src/pages/Analytics/Analytics.js
+++ src/pages/Analytics/Analytics.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 
 import Header from '../components/Header'
 
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, orderBy, limit, query } from 'firebase/firestore'
 import { database } from '../../firebaseConfig'
 
 import { useNavigate } from 'react-router-dom'
@@ -15,10 +15,12 @@ function Analytics() {
     const navigate = useNavigate();
 
     const [games, setGames] = useState([]);
+    const [maxDif, setMaxDif] = useState({})
 
     useEffect(() => {
         setGames([])
         getGames()
+        getHighestDif()
     }, [])
 
     const getGames = async () => {
@@ -34,36 +36,30 @@ function Analytics() {
         }
     }
 
-    // {
-    //     "time": "15:10",
-    //     "point_dif": 8,
-    //     "date": "31.07.2024",
-    //     "rounds": [
-    //         {
-    //             "player1": 4,
-    //             "round": 0,
-    //             "player2": 0
-    //         },
-    //         {
-    //             "round": 1,
-    //             "player2": 8,
-    //             "player1": 5
-    //         },
-    //         {
-    //             "player2": 9,
-    //             "round": 2,
-    //             "player1": 3
-    //         },
-    //         {
-    //             "player1": 3,
-    //             "player2": 6,
-    //             "round": 3
-    //         }
-    //     ],
-    //     "points_Lina": 23,
-    //     "winner": "Lina",
-    //     "points_Aaron": 15
-    // }
+
+    const getNumberOfWins = (player) => {
+        let wins = 0;
+        games.forEach(game => {
+            if (game.winner == player) {
+                wins++
+            }
+        })
+        return wins
+    }
+    console.log(maxDif)
+
+    const getHighestDif = async () => {
+        try {
+            setMaxDif(
+                (await getDocs(query(collection(database, 'games'), orderBy("point_dif", "desc"), limit(1))))
+                    .docs[0]
+                    .data()
+            )
+        } catch (error) {
+            console.error("Error fetching the game with highest pointDif: ", error);
+            return null;
+        }
+    }
 
     return (
         <div className='page'>
@@ -71,15 +67,25 @@ function Analytics() {
             <main>
                 <div className='statistics-main'>
                     <h2 className='statistics-main-subtitle'>Siege pro Person</h2>
-                    <h2>Aaron: </h2>
-                    <h2>Lina: </h2>
+                    <h2>Aaron: {getNumberOfWins('Aaron')}</h2>
+                    <h2>Lina: {getNumberOfWins('Lina')}</h2>
 
                     <h2 className='statistics-main-subtitle'>größte Punktedifferenz</h2>
+                    {maxDif &&
+                        <div className='game'>
+                            <div className='game-result'>
+                                <h3 className='game-result-aaron'>(Aaron) {maxDif.points_Aaron} </h3>
+                                <h3> : </h3>
+                                <h3 className='game-result-lina'> {maxDif.points_Lina} (Lina)</h3>
+                            </div>
+                            <p>{maxDif.date}, {maxDif.time}</p>
+                        </div>
+                    }
+
 
                     <h2 className='statistics-main-subtitle'>bisherige Spiele</h2>
                     {
                         games.map((game, index) => {
-                            console.log(game)
                             return (
                                 <div key={index} className='game'>
                                     <div className='game-result'>
